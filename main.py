ea18d65f474e913e723947353825cf49233e5242
Safwen <safwen.naimi@gmail.com>
2022-02-11 23:26:03 +0100

    noice

--- main.py
+++ main.py
@@ -10,3 +10,21 @@ import os
 import copy
 import cv2
 
+mean = np.array([0.5, 0.5, 0.5])
+std = np.array([0.25, 0.25, 0.25])
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean,std)
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+}
+
